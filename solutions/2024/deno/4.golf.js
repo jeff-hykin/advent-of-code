38147d9062c0b6a61d5b4d4c922dc7d7e5aa796e
Jeff Hykin <jeff.hykin@gmail.com>
2025-01-02 10:19:04 -0500

    shorten 4 and 5

--- solutions/2024/deno/4.golf.js
+++ solutions/2024/deno/4.golf.js
@@ -1,4 +1,3 @@
 new Response(Deno.stdin.readable).text().then((a,e=0,d=0,D,f,i)=>{
 for(i in a)[-142,-141,-140,-1,D=1,140,141,142].map(o=>(f=(c,l)=>a[c]=="XMAS"[l]&l<4?f(c+o,l+1):l>3,D+=~o&1*f(i-o,1),e+=f(+i,0))),d+=D>2
-console.log(e,d)
-})
+console.log(e,d)})
