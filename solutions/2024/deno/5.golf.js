6a0fda4e6345620f70a7f80ac37e055e5321ccf3
Jeff Hykin <jeff.hykin@gmail.com>
2025-01-02 10:07:19 -0500

    add day 5

---
+++ solutions/2024/deno/5.golf.js
@@ -0,0 +1,5 @@
+new Response(Deno.stdin.readable).text().then((t,v,u,s,o=[0,0],l=t.split`
+`)=>{
+l.map(z=>o[(v=[...u=z.split`,`].sort((a,b,f=x=>u.map(n=>s+=l.includes(n+"|"+x),s=0)|s)=>f(a)-f(b)))!=z|0]+=+v[v.length>>1]|0)
+console.log(...o)
+})
